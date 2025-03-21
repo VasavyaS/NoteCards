435f7b6d57be2173413b86ff77e46231693bcb6a
Vasavya <vasavyasritm@gmail.com>
2025-03-20 23:56:06 -0700

    enabled CORS

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ import os
 app = Flask(__name__)
 
 # We can comment this CORS config for the production because we are running the frontend and backend on the same server
-# CORS(app) 
+CORS(app) 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///friends.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -32,6 +32,10 @@ import routes
 with app.app_context():
   db.create_all()
 
+# if __name__ == "__main__":
+#   app.run(debug=True)
+
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=False)
+
 #This is CICD pipeline test, example.....
